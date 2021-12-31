85c6a242890c3141db7e288a41ccd8b0d088a9f8
david beltran <david.beltran2326@gmail.com>
2021-12-30 21:03:30 -0500

    add basic configuration project

---
+++ lib/home/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  State<HomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
